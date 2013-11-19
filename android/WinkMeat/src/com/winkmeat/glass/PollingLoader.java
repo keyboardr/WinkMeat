4b28d8b4d2707dcbf784f733bc3631facce71cc1
keyboardr <keyboardr@gmail.com>
2013-11-19 15:03:48 -0800

    Fix data type

--- android/WinkMeat/src/com/winkmeat/glass/PollingLoader.java
+++ android/WinkMeat/src/com/winkmeat/glass/PollingLoader.java
@@ -2,16 +2,17 @@ package com.winkmeat.glass;
 
 import android.content.Context;
 
+import com.winkmeat.glass.data.PollingResult;
 import com.winkmeat.glass.util.CachedLoader;
 
-public class PollingLoader extends CachedLoader<PollingLoader> {
+public class PollingLoader extends CachedLoader<PollingResult> {
 
 	public PollingLoader(Context context) {
 		super(context);
 	}
 
 	@Override
-	public PollingLoader loadInBackground() {
+	public PollingResult loadInBackground() {
 		// TODO Auto-generated method stub
 		return null;
 	}
