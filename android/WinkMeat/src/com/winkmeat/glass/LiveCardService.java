2fd0515a2188f5f487452780146ff8a63ebc52ca
keyboardr <keyboardr@gmail.com>
2013-11-19 17:40:49 -0800

    merge changes

--- android/WinkMeat/src/com/winkmeat/glass/LiveCardService.java
+++ android/WinkMeat/src/com/winkmeat/glass/LiveCardService.java
@@ -5,8 +5,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.IBinder;
+import android.widget.RemoteViews;
+
+import com.google.android.glass.timeline.LiveCard;
+import com.google.android.glass.timeline.TimelineManager;
+import com.winkmeat.glass.data.PollingResult;
 
 public class LiveCardService extends Service {
+
+	private static final String CARD_ID = "winkmeat_card";
 
 	public static void startService(Context context, Uri probeUri) {
 		// TODO
@@ -15,6 +22,29 @@ public class LiveCardService extends Service {
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
+	}
+
+	private LiveCard mLiveCard;
+
+	private void publishCard(Context context, PollingResult data) {
+		if (mLiveCard == null) {
+			TimelineManager timeMan = TimelineManager.from(context);
+			mLiveCard = timeMan.getLiveCard(CARD_ID);
+		}
+	}
+
+	private void bindView(Context context, LiveCard card, PollingResult data) {
+		RemoteViews view = new RemoteViews(context.getPackageName(),
+				R.layout.activity_main);
+
+	}
+
+	private void updateCard(Context context, PollingResult data) {
+		// TODO
+	}
+
+	private void removeCard(Context context) {
+		// TODO
 	}
 
 }
