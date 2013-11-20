c21a6b60039da4fe366ab12bbd068b37906f3173
keyboardr <keyboardr@gmail.com>
2013-11-19 18:39:49 -0800

    Pass off to LiveCardService naively.

--- android/WinkMeat/src/com/winkmeat/glass/util/StartupService.java
+++ android/WinkMeat/src/com/winkmeat/glass/util/StartupService.java
@@ -16,9 +16,13 @@ import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.winkmeat.glass.LiveCardService;
 import com.winkmeat.glass.data.PollingResult;
 
 public class StartupService extends IntentService {
+
+	private static final Uri LOCAL_URI = Uri
+			.parse("http://192.168.1.3/luci/lm/hmstatus");
 
 	public StartupService(String name) {
 		super(name);
@@ -26,33 +30,37 @@ public class StartupService extends IntentService {
 	}
 
 	public static final boolean USE_DEBUG = true;
-	
+
 	public static PollingResult getSmokerStatus(Uri uri) {
-		if (uri == null)
-			uri = Uri.parse("http://192.168.1.3/luci/lm/hmstatus");
-		
+		if (uri == null) {
+			uri = LOCAL_URI;
+		}
+
 		Gson gson = new Gson();
-		Type pollingResultType = new TypeToken<PollingResult>(){}.getType();
+		Type pollingResultType = new TypeToken<PollingResult>() {
+		}.getType();
 		String pollingResultJson = null;
-		if (USE_DEBUG)
+		if (USE_DEBUG) {
 			pollingResultJson = "{\"time\":1384903341,\"set\":225,\"lid\":0,\"fan\":{\"c\":0,\"a\":0},\"temps\":[{\"n\":\"Probe 0\",\"c\":74.3,\"a\":{\"l\":-40,\"h\":-200,\"r\":null}},{\"n\":\"Probe 1\",\"c\":80.3,\"a\":{\"l\":-40,\"h\":-200,\"r\":null}},{\"n\":\"Probe 2\",\"c\":72.2,\"a\":{\"l\":-40,\"h\":-200,\"r\":null}},{\"n\":\"Probe 3\",\"c\":88.9,\"a\":{\"l\":-40,\"h\":-200,\"r\":null}}]}";
-		else
+		} else {
 			pollingResultJson = getUrlResult(uri);
-		
+		}
+
 		return gson.fromJson(pollingResultJson, pollingResultType);
 	}
 
 	private static String getUrlResult(Uri uri) {
 		try {
 			URL url = new URL(uri.toString());
-			BufferedReader rd = new BufferedReader(new InputStreamReader(url.openStream()));
-		    String line=null;
-		    String message = new String();
-		    final StringBuffer buffer = new StringBuffer(2048);
-		    while ((line = rd.readLine()) != null) {
-		        buffer.append(line);
-		    }
-		    return buffer.toString();
+			BufferedReader rd = new BufferedReader(new InputStreamReader(
+					url.openStream()));
+			String line = null;
+			String message = new String();
+			final StringBuffer buffer = new StringBuffer(2048);
+			while ((line = rd.readLine()) != null) {
+				buffer.append(line);
+			}
+			return buffer.toString();
 		} catch (MalformedURLException e) {
 			Log.e("StartupService", "Bad server address");
 		} catch (UnknownHostException e) {
@@ -74,8 +82,6 @@ public class StartupService extends IntentService {
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
-		// TODO Auto-generated method stub
-		
+		LiveCardService.startService(this, LOCAL_URI);
 	}
-
 }
